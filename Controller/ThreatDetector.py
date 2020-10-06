b1d5409b4cbc9543b1ee5c004729633d245bb521
Junhyeok Yun <junhyeok.dev@gmail.com>
2020-10-06 13:48:29 +0900

    feat: Add flow rule generator

--- Controller/ThreatDetector.py
+++ Controller/ThreatDetector.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import requests
 
 
 class DetectorThread(threading.Thread):
@@ -27,13 +28,18 @@ class DetectorThread(threading.Thread):
                 {"timestamp": "2020-9-8 15:58", "protocol": "tcp", "src": "10.0.0.2", "dst": "10.0.0.1"}
             ]
 
-            print(detectIpEntropyOutlier(packetdata, 2, 1))
+            atts = detectIpEntropyOutlier(packetdata, self.thsrc)
+
+            for att in atts:
+                generateFlowRule(att, "192.168.0.2")
 
             time.sleep(self.thtime)
 
-def detectIpEntropyOutlier(packets, thsrc, thtime): #패킷데이터, 임계치, 단위시간 입력
+def detectIpEntropyOutlier(packets, thsrc): #패킷데이터, 임계치, 단위시간 입력
     dstIps = [] #목적지 주소를 저장하는 배열
     dstSrcMap = {} #목적지 - 목적지로 패킷을 보낸 출발지를 저장하는 딕셔너리
+    attackers = []
+
     [dstIps.append(packet["dst"]) if packet["dst"] not in dstIps else 0 for packet in packets] #dspIps(목적지 주소 리스트) 생성 루프
     for dstIp in dstIps:
         dstSrcMap[dstIp] = [] #dstSrcMap 딕셔너리 초기화
@@ -41,14 +47,38 @@ def detectIpEntropyOutlier(packets, thsrc, thtime): #패킷데이터, 임계치,
     #dstSrcMap 생성 루프
     [dstSrcMap[packet["dst"]].append(packet["src"]) if packet["src"] not in dstSrcMap[packet["dst"]] else 0 for packet in packets]
 
-    result = False #결과를 False로 초기화
-
     print(dstSrcMap)
     for key, val in dstSrcMap.items():
         if len(val) > thsrc:
-            result = True
+            attackers.append(key)
 
-    return result
+    return attackers
+
+
+def generateFlowRule(att, honeypot_url):
+    rule = {
+        "priority": 40000,
+        "timeout": 0,
+        "isPermanent": True,
+        "deviceId": "of:0000000000000001",
+        "treatment": {
+            "instructions": [
+                {
+                    "type": "OUTPUT",
+                    "port": "CONTROLLER"
+                }
+            ]
+        },
+        "selector": {
+            "criteria": [
+                {
+                    "type": "ETH_TYPE",
+                    "ethType": "0x88cc"
+                }
+            ]
+        }
+    }
+    resp = requests.post("http://localhost:8181/onos/v1/flows/", data=rule)
 
 
 dtThread = DetectorThread(2, 3)
