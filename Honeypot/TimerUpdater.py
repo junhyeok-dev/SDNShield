7ca9b8a100913739fa24ad46ca5cbf9a21c8bcaf
Junhyeok Yun <junhyeok.dev@gmail.com>
2020-10-27 21:45:13 +0900

    feat: Get timestamp from TShark

--- Honeypot/TimerUpdater.py
+++ Honeypot/TimerUpdater.py
@@ -12,14 +12,14 @@ class TLMCommunicator(threading.Thread):
     def __init__(self, c_ip):
         super().__init__()
         self.controller_ip = c_ip
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP 사용
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def run(self):
-        self.sock.connect((self.controller_ip, 58678)) # 연결 요청
+        self.sock.connect((self.controller_ip, 58678))
 
-        self.sock.send("init".encode()) # 값 전송
+        self.sock.send("init".encode())
 
-        dat = self.sock.recv(1024).decode() # 값 읽기
+        dat = self.sock.recv(1024).decode()
         print("Received: ", dat)
 
     def updateTimer(self, min, hr):
@@ -56,41 +56,40 @@ time.sleep(0.1)
 packetdata =[]
 
 capture = subprocess.Popen([
-    'tshark', '-l', '-i', interface, '-Tfields', '-e', 'ip.src', '-e', 'ip.dst',
+    'tshark', '-l', '-i', interface, '-Tfields',
+    '-e', 'frame.time_relative','-e', 'ip.src', '-e', 'ip.dst',
     '-e', 'tcp.srcport', '-e', 'tcp.flags'
 ], stdout=subprocess.PIPE)
 
+linenum = 0
+
 while True:
     line = capture.stdout.readline().decode().split("\t")
+    linenum += 1
     #print("Line: ", line)
 
     if len(line) > 3:
         packetdata.append({
-            "timestamp": time.time(), "src": line[0], "dst": line[1], "sport": line[2],
-            "flags": line[3]
+            "timestamp": float(line[0]), "src": line[1], "dst": line[2], "sport": line[3],
+            "flags": line[4]
         })
-
-
-        #시간 데이터를 UNIXTIME으로 변환하는 코드
+        
+    if linenum % 2000 == 0:
         timestamps = [packet["timestamp"] for packet in packetdata]
-        #모든 패킷 사이의 시간 간격을 리스트화
         timediffs = [(timestamps[i + 1] - timestamps[i]) / 60 for i in range(len(timestamps) - 1)]
-        print("Timediffs: ", timediffs)
 
         if len(timediffs) == 0: continue
-        #시간 간격의 평균 계산
         avg_diff = sum(timediffs) / len(timediffs)
 
         candidates = [timediff if timediff > avg_diff else 0 for timediff in timediffs]
-        print("Candidates: ", candidates)
         num_candidate = 0
         for candidate in candidates:
             if candidate != 0:
-                num_candidate += 1 # candidates의 개수 세기
+                num_candidate += 1
         candidates = sum(candidates)
 
         if num_candidate == 0: continue
-        new_timeout = int(candidates / num_candidate + c) # 새로운 타임아웃값 생성
+        new_timeout = int(candidates / num_candidate + c)
         print("New timeout: ", new_timeout)
 
-        tlmCommunicator.updateTimer(new_timeout, 60) # 허니팟으로 새로운 타임아웃값 전송
+        tlmCommunicator.updateTimer(new_timeout, 60)
